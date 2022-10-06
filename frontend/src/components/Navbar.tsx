3473e38dc8a68cb434ab8f03cbf8213d88af798f
Matthew Yasul <yasulmatthew@gmail.com>
2022-10-06 08:56:13 +0800

    Add note in Navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -13,6 +13,7 @@ export const Navbar = () => (
                     </a>
                 </div>
                 <div className="hidden sm:block">
+                    {/* NOTE: Can implement dropdown if the buttons' functions are implemented. */}
                     <button type="button" className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-whitehover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-500" aria-expanded="false">
                         <span className="sr-only">Open menu</span>
                         <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
